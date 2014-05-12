1183d4d97916821d356c5566da4e4d9a3af15a17
JonKerridge <jmandsdk@btinternet.com>
2014-05-12 18:44:11 +0100

    initial commit

---
+++ demos.go
@@ -0,0 +1,16 @@
+// demos
+package main
+
+import (
+	"fmt"
+	//	"github.com/JonKerridge/pnp"
+)
+
+func main() {
+	fmt.Printf("Concurrent and Parallel Systems in GO\n")
+	fmt.Printf("Jon Kerridge Edinburgh Napier Univerity\n")
+	fmt.Printf("email : j dot kerridge at napier.ac.uk\n")
+	fmt.Printf(" code : https://github.com/JonKerridge\n\n\n")
+
+	fmt.Printf("A Set of Demo Networks Showing the Operation of Package pnp\n")
+}
